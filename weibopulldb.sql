04beeea6eba8e93b903e7689e5ab5373ae02b6fa
Xiaokang Wang <xiaokangwang@outlook.com>
2015-11-27 10:34:16 +0800

    Database Structure

---
+++ weibopulldb.sql
@@ -0,0 +1,3 @@
+CREATE TABLE "programma_configure" ("confname" TEXT, "cfncval" );
+CREATE TABLE "weibocrawtarget" ("typeofowner" TEXT, "uid" TEXT, "crawedtime" DATETIME);
+CREATE TABLE "weibofeeds" ("uid" TEXT, "textw" TEXT, "page" INTEGER);
