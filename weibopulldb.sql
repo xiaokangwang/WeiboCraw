23cca653617673a3ccdee74702d6633ed20ec100
Shelikhoo <xiaokangwang@outlook.com>
2015-11-28 09:06:29 +0800

    DB structure

--- weibopulldb.sql
+++ weibopulldb.sql
@@ -1,3 +1,6 @@
+CREATE TABLE "crawbuffer" ("uid" TEXT,"time" INTEGER,"crawinstanceuuid" TEXT,"firedurl" TEXT,"ctx" TEXT,"pageno" INTEGER,"Nextpagefound" BOOL);
+CREATE TABLE "crawinstance" ("crawinstanceuuid" TEXT, "initializedtime" INTEGER, "crawas" TEXT, "crawcookie" TEXT, "crawnote" TEXT);
+CREATE TABLE "crawresult" ("uid" TEXT,"lasttime" INTEGER DEFAULT (null) ,"lastcrawedpage" INTEGER DEFAULT (0) );
 CREATE TABLE "programma_configure" ("confname" TEXT, "cfncval" );
-CREATE TABLE "weibocrawtarget" ("typeofowner" TEXT, "uid" TEXT, "crawedtime" DATETIME);
-CREATE TABLE "weibofeeds" ("uid" TEXT, "textw" TEXT, "page" INTEGER);
+CREATE TABLE "weibocrawtarget" ("typeofowner" TEXT,"uid" TEXT);
+CREATE TABLE "weibofeeds" ("uid" TEXT, "textw" TEXT, "page" INTEGER,"natpage" INTEGER);
